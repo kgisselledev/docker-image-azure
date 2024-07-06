364fee742fb3f6d0c9246e9eb2b900776c9a8f15
kgisselledev <slayerdragonland@gmail.com>
2024-07-06 00:55:11 -0500

    Comit AKS

---
+++ src/main/java/com/dockerazr/dockerApplication.java
@@ -0,0 +1,11 @@
+package com.dockerazr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class dockerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(dockerApplication.class, args);
+    }
+}
